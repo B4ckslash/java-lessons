ef52c0772eba108165bd393196ac050b438ea04b
B4ckslash <moral1tycor3@gmail.com>
2018-10-27 10:53:01 +0200

    Added Slides and sourcefiles for first lesson

---
+++ sourcefiles/01-introduction/src/Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static void main(String[] args) {
+        float a = 2f;
+        float b = 346364.1f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a*b);
+    }
+}
